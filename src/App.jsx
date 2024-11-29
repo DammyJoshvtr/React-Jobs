7cf0f2fcc90066e233a59194783d96910f6a2e6d
Damilola Joshua <damilolajoshua2021@gmail.com>
2024-11-29 12:37:05 -0800

    Testing JSX and Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
 const App = () => {
+
+  const name= 'Damilola';
+  const x = 10;
+  const y = 20;
+  const names = ['Brad', 'Mary', 'Joe', 'Sara']
+  const loggedIn = true
+  const styles = {
+    color: 'red',
+    fontSize: '55px'
+  }
+
+
   return (
-    <div>App</div>
+    <>
+      <div className="text-5xl">App</div>
+      <p style={ styles }>My name is {name}</p>
+      <p>
+        The Sum of {x} and {y} is {x + y}
+      </p>
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+      {loggedIn ? <h1>Hello Member</h1> : <h1>Hello Guest</h1>}
+    </>
   )
 }
 
