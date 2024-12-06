e26f0f228549f2daab13a5a12f55c57dd20638b8
Damilola Joshua <damilolajoshua2021@gmail.com>
2024-12-06 00:09:02 -0800

    Added Pages Folder  installed react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,16 @@ import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
 import ViewAllJobs from "./components/ViewAllJobs";
- 
-const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-  
-    </>
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route index element={<h1>My App</h1>}/>
   )
+)
+
+const App = () => {
+  return <RouterProvider router={router} />
 }
 
 export default App
