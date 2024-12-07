57ef24bef44ce06b39af60f25d558b58b06034e5
Damilola Joshua <damilolajoshua2021@gmail.com>
2024-12-06 22:13:28 -0800

    Used Layouts to Create the Hompage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import Navbar from "./components/Navbar"; 
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { 
+  Route,
+  createBrowserRouter, 
+  createRoutesFromElements,
+  RouterProvider 
+} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route index element={<h1>My App</h1>}/>
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={ <HomePage /> } />
+    </Route>
   )
-)
+);
 
 const App = () => {
   return <RouterProvider router={router} />
